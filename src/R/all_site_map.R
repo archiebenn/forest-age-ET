6e024a14d90ae889df15fcdf31b62480fa82c279
archiebenn <archiesnbenn@gmail.com>
2026-05-30 10:09:13 +0100

    worldmap

--- src/R/all_site_map.R
+++ src/R/all_site_map.R
@@ -1,8 +1,6 @@
 # all_site_map.R - display flux tower sites on map
 # Author: Archie Benn
 
-#Sys.setenv(PROJ_LIB = "/home/ab/micromamba/envs/bbinf_project/share/proj")
-
 library(tidyverse)
 library(sf)
 library(rnaturalearth)
@@ -15,11 +13,18 @@ sites = read_csv("data/fluxnet/site_selection/site_ages.csv")
 # get world data
 world <- ne_countries(returnclass = "sf")
 
+# convert site long/lat for robinson projection
+sites_coords_robin <- sites %>%
+    st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 4326) %>%
+    st_transform(crs = "+proj=robin")
+
 # plot
 world %>%
     st_transform(crs = "+proj=robin") %>%
     ggplot() +
-    geom_sf() +
-    theme_minimal()
- 
+    geom_sf(fill = "grey90", colour = "grey75", linewidth = 0.2) + 
+    geom_sf(data = sites_coords_robin, aes(colour = SITE_AGE)) +
+    scale_colour_gradient(low = "lightgreen", high = "darkgreen", name = "Stand age (years)") +
+    theme_light()
+   
     
