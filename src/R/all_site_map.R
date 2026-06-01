d5f682b36ec9a74baec104f0c729a4fa29534b59
archiebenn <archiesnbenn@gmail.com>
2026-06-01 09:09:07 +0100

    diary

--- src/R/all_site_map.R
+++ src/R/all_site_map.R
@@ -20,16 +20,25 @@ sites_coords_robin <- sites %>%
     st_transform(crs = "+proj=robin")
 
 # plot
-tikz("diss/figures/world_sites.tex", width = 6, height = 4)
+tikz("diss/figures/world_sites.tex", width = 6, height = 5)
 
 world %>%
     st_transform(crs = "+proj=robin") %>%
     ggplot() +
     geom_sf(fill = "grey90", colour = "grey75", linewidth = 0.2) + 
     geom_sf(data = sites_coords_robin, aes(colour = SITE_AGE)) +
-    scale_colour_gradient(low = "lightgreen", high = "darkgreen", name = "Stand age (years)") +
-    theme_light()
-   
+    scale_colour_gradient(
+        low = "lightgreen", high = "darkgreen",
+        name = "Stand Age (Years)",
+        breaks = c(0, 100, 200, 300),
+        labels = c("0", "100", "200", "300")
+    ) +
+    theme_void() +
+    theme(legend.text = element_text(size = 8),
+          legend.title = element_text(size = 8),
+          legend.margin = margin(r = 10)) 
+
 dev.off()
 
 print("Done")
+
