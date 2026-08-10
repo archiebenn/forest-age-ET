8aaba75bc57838b80eef8c6e5372027aa035cd0a
archiebenn <archiesnbenn@gmail.com>
2026-08-10 15:20:40 +0100

    finished analysis 2 script

--- scripts/python/23_analysis_2.py
+++ scripts/python/23_analysis_2.py
@@ -23,12 +23,6 @@ timestamp = datetime.now().strftime("%d%m_%H%M")
 
 df = pd.read_csv("data/main/14_pre_processing/df_ml_ready.csv")
 df_sites = pd.read_csv("data/main/21_analysis_1.1/cleaned_sites.csv")
-df_gower = pd.read_csv("data/main/20_gower/gower_matrix.csv", index_col=0)
-df_cluster_labels = pd.read_csv("data/main/20_gower/df_cluster_labels.csv")
-
-# all sites list
-all_sites = df_sites["Site_ID"].astype(str).tolist()
-
 
 
 # *********************************************
@@ -52,10 +46,173 @@ def train_test_8020(site_df):
     # row id where 80% of rows in df lies
     split_id = int(len(site_df) * 0.8)
 
+    # earliest 80% and latest 20% 
     train_80 = site_df.iloc[:split_id]
     test_20 = site_df.iloc[split_id:]
 
     return train_80, test_20
 
 
-# 3.2 function to fit and predict RF models
+# 3.2 function to fit RF models with training data
+def rf_train_temporal(train_df, features_age, features_no_age, target = "ET"):
+
+    #params defined in script 15
+    params = {
+            "n_estimators": 250, 
+            "max_depth": 10, 
+            "min_samples_leaf": 10,
+            "max_features": 0.6
+        }
+
+    # 1. AGE MODEL (full features)
+    rf_age = RandomForestRegressor(
+        # use parameters defined above (** expands dict)
+        **params,
+        random_state=42,
+        # use all except one core
+        n_jobs=-2)
+    # fit
+    rf_age.fit(train_df[features_age], train_df[target])
+
+    # 2. NO AGE MODEL
+    rf_no_age = RandomForestRegressor(
+        # use parameters defined above (** expands dict)
+        **params,
+        random_state=42,
+        # use all except one core
+        n_jobs=-2)
+    # fit
+    rf_no_age.fit(train_df[features_no_age], train_df[target])
+
+    # return the fitted models
+    return rf_age, rf_no_age
+
+
+# 3.3 function to predict and return metrics and raw preds 
+def rf_pred(age_model, no_age_model, test_df, features_age, features_no_age, target = "ET"):
+
+    # predict!
+    preds_age = age_model.predict(test_df[features_age])
+    preds_no_age = no_age_model.predict(test_df[features_no_age])
+
+
+    # metrics
+    rmse_age = np.sqrt(mean_squared_error(test_df[target], preds_age))
+    rmse_no_age = np.sqrt(mean_squared_error(test_df[target], preds_no_age))
+    r2_age = r2_score(test_df[target], preds_age)
+    r2_no_age = r2_score(test_df[target], preds_no_age)
+
+    # return dict of metrics
+    return {
+        "rmse_age": rmse_age,
+        "rmse_no_age": rmse_no_age,
+        "r2_age":r2_age,
+        "r2_no_age": r2_no_age,
+        "preds_age": preds_age,
+        "preds_no_age": preds_no_age
+    }
+
+
+
+
+# *********************************************
+# 4. SETUP FEATURES
+# *********************************************
+# features to exluce in all instances
+non_features = [
+                    'Site_ID',       
+                    'Latitude',
+                    'Longitude',
+                    'Age_range',
+                    'ET',
+                    'LE',
+                    'P',              # have cumulative sum already
+                    'Pa',             # dropped
+                    'Date',
+                    'Unnamed: 0',     # not sure where this came from
+
+                    # the following categorical columns will all be dropped as a test 9-8-26 (from 1.1 analysis)
+                    "Continent_Europe",
+                    "Continent_North America",
+                    "Cover_type_DBF",
+                    "Cover_type_EBF",
+                    "Cover_type_ENF",
+                    "Cover_type_MF",
+                    "Cover_type_OSH",
+                    "Climate_zone_Continental",
+                    "Climate_zone_Polar",
+                    "Climate_zone_Temperate",
+                    "Climate_zone_Tropical"
+                    ]
+
+
+# age column to drop for the no age model
+age_column = 'Site_age'
+
+features_age = [c for c in df_cleaned.columns if c not in non_features]
+features_no_age = [c for c in df_cleaned.columns
+                    if c not in non_features and c != age_column]
+
+
+
+# *********************************************
+# 5. MAIN LOOP
+# *********************************************
+results = []
+predictions_full = []
+
+# loop over each site in the cleaned df
+for site in sites_long["Site_ID"]:
+
+    # 1. set site as main df and sort by date (should already be in order but just to be safe)
+    df_site = df_cleaned[df_cleaned["Site_ID"] == site].sort_values("Date").reset_index(drop=True)
+
+    # 2. setup train/test split
+    train_df, test_df = train_test_8020(site_df=df_site)
+
+    # 3. fit age and no age models
+    rf_age, rf_no_age = rf_train_temporal(train_df=train_df,
+                                          features_age=features_age,
+                                          features_no_age=features_no_age)
+
+    # 4. compute preds and metrics
+    metrics = rf_pred(age_model=rf_age,
+                      no_age_model=rf_no_age,
+                      test_df=test_df,
+                      features_age=features_age,
+                      features_no_age=features_no_age)
+
+    # 5. save out metrics to results
+    results.append({
+        "test_site": site,
+        "test_site_cluster": df_sites.loc[df_sites["Site_ID"] == site, "cluster"].values[0],
+        "rmse_age": metrics["rmse_age"],
+        "rmse_no_age": metrics["rmse_no_age"],
+        "r2_age": metrics["r2_age"],
+        "r2_no_age": metrics["r2_no_age"],
+    })
+
+    # save predictions (large table)
+    for i in range(len(test_df)):
+        predictions_full.append({
+        "test_site": site,
+        "test_site_cluster": df_sites.loc[df_sites["Site_ID"] == site, "cluster"].values[0],
+        "timestamp": test_df.iloc[i]["Date"],
+        "y_true": test_df.iloc[i]["ET"],
+        "pred_age": metrics["preds_age"][i],
+        "pred_no_age": metrics["preds_no_age"][i],
+        "rmse_age": metrics["rmse_age"],
+        "rmse_no_age": metrics["rmse_no_age"],
+        "r2_age": metrics["r2_age"],
+        "r2_no_age": metrics["r2_no_age"],
+    })
+
+    print(f"Temporal predictions and metrics for {site} complete")
+    
+
+# save out results and predictions
+results_df = pd.DataFrame(results)
+predictions_df = pd.DataFrame(predictions_full)
+
+results_df.to_csv(f"data/main/23_analysis_2/metrics_{timestamp}.csv", index=False)
+predictions_df.to_csv(f"data/main/23_analysis_2/predictions_{timestamp}.csv", index=False)
