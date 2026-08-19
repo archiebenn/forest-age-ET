5e5401c3466867000a41c9fab9f8a9fc939775cd
archiebenn <archiesnbenn@gmail.com>
2026-08-19 19:55:23 +0100

    diss stuff etc.

---
+++ scripts/python/29_analysis_1.3.py
@@ -0,0 +1,4 @@
+# analysis_1.3.py - part 1.3 of final analysis for dissertation
+# this script builds rf models (with and without age) trained by cluster (minus medoids) and predicts on the 6 medoid (representative) test sites, before saving metrics
+# date: 
+# author: Archie Benn sj19031@bristol.ac.uk
